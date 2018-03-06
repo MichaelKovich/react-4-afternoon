36e01895b176002af17f63bc80ab49d42063dbab
Michael Kovich <kovich@michaelkovich.com>
2018-03-06 15:09:05 -0600

    Step 5 complete.

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,14 +5,14 @@ export default class Home extends Component {
   render() {
     return (
       <div className="box">
-        <Link to="/classlist/MATH1010" className="btn">
-          Math 1010
+        <Link to="/classlist/MATH1010">
+          <button className="btn">Math 1010</button>
         </Link>
-        <Link to="/classlist/ENGLISH2010" className="btn">
-          English 2010
+        <Link to="/classlist/ENG2010">
+          <button className="btn">English 2010</button>
         </Link>
-        <Link to="/classlist/BIOLOGY2020" className="btn">
-          Biology 2020
+        <Link to="/classlist/BIO2020">
+          <button className="btn">Biology 2020</button>
         </Link>
       </div>
     );
--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,4 @@
-// Configure a router in this file:
-// Use the following components as routes:
-//   ./src/components/Home/Home.js
-//   ./src/components/About/About.js
-// Use the following combinations of paths and components for your routes:
-//   Path: "/" - Component: Home - This path should be exact.
-//   Path: "/about" - Component: About.
-
-import React, {Component} from 'react';
+import React from 'react';
 import {Switch, Route} from 'react-router-dom';
 import Home from './components/Home/Home';
 import About from './components/About/About';
@@ -16,6 +8,6 @@ export default (
   <Switch>
     <Route component={Home} exact path="/" />
     <Route component={About} path="/about" />
-    <Route component={ClassList} path="/classlist" />
+    <Route component={ClassList} path="/classlist/:class" />
   </Switch>
 );
